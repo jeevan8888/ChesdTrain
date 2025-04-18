7a75c132ad46773841e68d3c33f11ef9a0561803
jeevan8888 <johnwilliam199024@gmail.com>
2025-04-19 01:29:10 +0800

    Update: Train_controller

--- app/controllers/train_controller.rb
+++ app/controllers/train_controller.rb
@@ -17,7 +17,12 @@ class TrainController < ApplicationController
     render 'train/chested/step2'
   end
 
-  def c_step2_save  
+  def c_step2_save
+    session[:street_address] = params[:street_address]
+    session[:address] = params[:address]
+    session[:city] = params[:city]
+    session[:state] = params[:state]
+    
     redirect_to train_c_step3_path
   end
 
@@ -26,6 +31,10 @@ class TrainController < ApplicationController
   end
   
   def c_step3_save
+    session[:meal_preferences] = params[:meal_preferences]
+    session[:dietary_restrictions] = params[:dietary_restrictions]
+    session[:allergies] = params[:allergies]
+    
     redirect_to train_c_step4_path
   end
 
@@ -34,7 +43,13 @@ class TrainController < ApplicationController
   end
 
   def c_step4_save
-    redirect_to train_c_step5_path
+    session[:start_date] = params[:start_date]
+    session[:end_date] = params[:end_date]
+    session[:notes] = params[:notes]
+    
+    # Here you would typically save all the collected data to the database
+    # For now, we'll just redirect with a success message
+    redirect_to train_path, notice: "Chesed Train created successfully!"
   end
 
   def p_step1
@@ -42,6 +57,9 @@ class TrainController < ApplicationController
   end
 
   def p_step1_save
+    session[:potluck_name] = params[:potluck_name]
+    session[:potluck_email] = params[:potluck_email]
+    
     redirect_to train_p_step2_path
   end
 
@@ -50,6 +68,11 @@ class TrainController < ApplicationController
   end
 
   def p_step2_save
+    session[:potluck_location] = params[:potluck_location]
+    session[:potluck_address] = params[:potluck_address]
+    session[:potluck_city] = params[:potluck_city]
+    session[:potluck_state] = params[:potluck_state]
+    
     redirect_to train_p_step3_path
   end
 
@@ -58,6 +81,10 @@ class TrainController < ApplicationController
   end
 
   def p_step3_save
+    session[:potluck_date] = params[:potluck_date]
+    session[:potluck_time] = params[:potluck_time]
+    session[:potluck_type] = params[:potluck_type]
+    
     redirect_to train_p_step4_path
   end
 
@@ -66,6 +93,11 @@ class TrainController < ApplicationController
   end
 
   def p_step4_save
-    redirect_to train_p_step5_path
+    session[:potluck_items_needed] = params[:potluck_items_needed]
+    session[:potluck_notes] = params[:potluck_notes]
+    
+    # Here you would typically save all the collected data to the database
+    # For now, we'll just redirect with a success message
+    redirect_to train_path, notice: "Potluck Train created successfully!"
   end
 end
